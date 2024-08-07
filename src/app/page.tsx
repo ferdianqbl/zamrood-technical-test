267a9cab1979113cef084471f58ecade01c2132d
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-08-08 01:30:02 +0700

    feat: add destination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import Destination from "@/components/pages/home/destination";
 import Discover from "@/components/pages/home/discover";
 import Experience from "@/components/pages/home/experience";
 import Hero from "@/components/pages/home/hero";
+import Image from "next/image";
 
 const Page = () => {
   return (
@@ -8,12 +10,24 @@ const Page = () => {
       <section className="hero-image w-full min-h-screen" id="home_hero">
         <Hero />
       </section>
-      <section className="py-20" id="home_experience">
+      <section className="pt-20" id="home_experience">
         <Experience />
       </section>
-      <section className="pt-10 pb-20" id="home_discover">
+      <section className="pt-20" id="home_discover">
         <Discover />
       </section>
+      <div className="pt-20">
+        <Image
+          src="/assets/icons/ic-separator.svg"
+          width={1000}
+          height={1000}
+          alt="ornament"
+          className="mx-auto"
+        />
+      </div>
+      <div className="pt-20">
+        <Destination />
+      </div>
     </main>
   );
 };
