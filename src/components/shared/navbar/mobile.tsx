4b03111b856c76427d940e99691a68c3a7693b8c
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-08-08 00:12:00 +0700

    feat: responsive navbar;

--- src/components/shared/navbar/mobile.tsx
+++ src/components/shared/navbar/mobile.tsx
@@ -1,23 +1,18 @@
 "use client";
-import { FC } from "react";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { FC, useState } from "react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   scrollY: number;
 };
 
 const NavbarMobile: FC<Props> = ({ scrollY }) => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="lg:hidden container flex items-center justify-between w-full gap-3">
       <Link href="/" className="block">
@@ -32,7 +27,7 @@ const NavbarMobile: FC<Props> = ({ scrollY }) => {
           sizes="30vw"
         />
       </Link>
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger
           className={cn(
             "border rounded-full border-primary-400 text-primary-400 p-2",
@@ -41,14 +36,41 @@ const NavbarMobile: FC<Props> = ({ scrollY }) => {
         >
           <MenuIcon className="w-5 h-5" />
         </SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetContent className="">
+          <div
+            className={cn(
+              "mt-10 flex flex-col items-center gap-8 text-primary-400  justify-center"
+            )}
+          >
+            <Link
+              href="/#home-hero"
+              className={cn("border-b border-primary-400 pb-2 font-medium")}
+              onClick={() => setIsOpen(false)}
+            >
+              Homepage
+            </Link>
+            <Link
+              href="/#tailored-experiences"
+              className=" hover:border-b hover:border-basic-100"
+            >
+              Customize Your Trip
+            </Link>
+            <Link href="/destination" className="">
+              Destination
+            </Link>
+            <Link href="/#article" className="">
+              Article
+            </Link>
+            <Link href="/t">
+              <Button
+                className={cn(
+                  "border rounded-full bg-transparent hover:bg-primary-500 hover:border-primary-500 hover:font-medium border-primary-400 text-primary-400 hover:text-basic-100"
+                )}
+              >
+                Need Assitance?
+              </Button>
+            </Link>
+          </div>
         </SheetContent>
       </Sheet>
     </div>
