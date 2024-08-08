2efc1bb8e3dc6efc991e229bcffee085be983aae
ferdianqbl <muhammadferdianiqbal@gmail.com>
2024-08-08 13:01:52 +0700

    feat: create url to pandooin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Articles from "@/components/pages/home/articles";
 import Destination from "@/components/pages/home/destination";
 import Discover from "@/components/pages/home/discover";
 import Experience from "@/components/pages/home/experience";
 import Hero from "@/components/pages/home/hero";
+import { ArrowUpRightIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Page = () => {
   return (
@@ -27,6 +30,42 @@ const Page = () => {
       </div>
       <div className="pt-20">
         <Destination />
+      </div>
+      <div className="container mt-20">
+        <div
+          className="flex justify-between py-8 px-4 relative"
+          style={{
+            backgroundImage: `linear-gradient(0deg, rgba(0, 0, 0, 0.36) 0%, rgba(0, 0, 0, 0.36) 100%), url('/assets/images/img-background.png')`,
+            width: "100%",
+            height: "100%",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <Image
+            src={"/assets/icons/ic-company-logo.svg"}
+            alt="ic-company"
+            width={135}
+            height={50}
+            loading="eager"
+            sizes="30vw"
+          />
+          <div className="flex flex-col text-basic-100">
+            <span>
+              Want to see other destinations? Check us out at our website
+            </span>
+            <Link
+              href={"https://pandooin.com"}
+              target="_blank"
+              className="flex items-center gap-1 text-end w-full justify-end font-medium text-xl underline"
+            >
+              pandooin.com <ArrowUpRightIcon className="w-6 h-6" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="py-20">
+        <Articles />
       </div>
     </main>
   );
---
+++ src/components/pages/home/articles/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Articles = () => {
+  return (
+    <div className="container">
+      <h3 className="text-3xl text-center lg:text-start text-primary-400 font-bold font-unbounded">
+        Articles
+      </h3>
+      <p className="text-xl text-center lg:text-start text-primary-400">
+        Our curated writings, offering something for every reader.
+      </p>
+    </div>
+  );
+};
+
+export default Articles;
